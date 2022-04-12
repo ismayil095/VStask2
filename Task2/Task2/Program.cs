cf07593529f0c8072b2f6aa36c8540ae6008e92c
ismayil095 <isimma095@gmail.com>
2022-04-12 17:18:32 +0400

    init

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Eded cutdur.");
+            }
+            else
+            {
+                Console.WriteLine("Eded tekdir.");
+            }
+        }
+    }
+}
